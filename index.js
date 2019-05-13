e7d0bd9d296ce38f30a531efd765181a2676cda4
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-05-13 03:04:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,5 @@
 let num1 = 31, num2 = 2;
 let multiply= num1 * num2;
 let random = (Math.random() * 10) + 1;
-let mod = 19 % 3;
-let max = [1, 4, 5, 20];
-max.max();
+let mod = 19 % 5;
+let max = Math.max(1, 4, 5, 20);
